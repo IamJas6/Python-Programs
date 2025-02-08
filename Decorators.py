400e744486df2fb218d928720dadf15a637660d2
iamjas6 <suhailuddin2002@gmail.com>
2025-02-08 19:05:51 +0530

    Decorators Committed

---
+++ Decorators.py
@@ -0,0 +1,36 @@
+#User defined decorators
+#syntax:
+    #def dname(func):
+        #def inner():
+            #operation before main task
+            #func()
+            #operation after main task
+        #return inner
+    #@dame
+    #def fname(args):
+    #   statement
+
+#1.WAP to calculate total time taken to find square of the numbers from range 1 to 100
+# import time
+# st = time.time()
+# for i in range(1,101):
+#     print(i)
+# et = time.time()
+# print(et-st)
+
+#2. WAP to calculate total time taken to print the sum of 2 integers
+# def timer(func):
+#     def inner():
+#         import time
+#         st = time.time()
+#         func()
+#         et = time.time()
+#         print(et-st)
+#     return inner
+
+# @timer
+# def sum():
+#     n1=int(input('enter number1: '))
+#     n2=int(input('enter number2: '))
+#     print(n1+n2)
+# sum()
