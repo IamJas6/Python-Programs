16913ac25cd9e5436cb389942b25270df0b99df6
iamjas6 <suhailuddin2002@gmail.com>
2025-02-15 23:26:57 +0530

    Enumerate Committed

---
+++ Enumerate.py
@@ -0,0 +1,15 @@
+items = ['apple', 'orange', 'banana','apple']
+
+#One method
+# for i in range(len(items)):
+#     print(f'{i+1} -> {items[i]}')
+
+#Second method
+# i=1
+# for item in items:
+#     print(f'{i} -> {item}')
+#     i+=1
+
+#Enumerate Method
+# for index, item in enumerate(items, start=1):
+#     print(f'{index} -> {item}')
