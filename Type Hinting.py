6e0dab0ef86f0c747001ee58fa68a8ae6fca0d20
iamjas6 <suhailuddin2002@gmail.com>
2025-02-15 22:51:16 +0530

    Type Hinting Committed

---
+++ Type Hinting.py
@@ -0,0 +1,18 @@
+from typing import List
+
+#This is normal function 
+# def add(a,b):
+#     return a+b
+# print(add(1,2))
+
+
+#This is type hinting function 
+# def add(a:int,b:int):  #here we mention which datatype should be used declared using colom :
+#     return a+b
+# print(add(1,2))
+
+
+#This is list type hinting function 
+# def add(a:List[int] ,b:int):  #here we mention which datatype should be used declared using colom : it for list
+#     return a+b
+# print(add(1,2))
