072b128f57266ee00588fe50070176378e3d9a3a
iamjas6 <suhailuddin2002@gmail.com>
2025-02-15 22:44:35 +0530

    Generators Yield Committed

---
+++ Generators Yield.py
@@ -0,0 +1,6 @@
+# def generate_numbers(n):  #Doesn't store in memory this makes it memory effiecient 
+#     for i in range(n):    #whereas other for loops store in memory
+#         yield i
+    
+# numbers = generate_numbers(1000000)
+# print(sum(numbers))
